073f3118b4bbb9658dac99f8b0316cd78c59257c
ganeodolu <ganeodolu@gmail.com>
2023-07-10 22:34:22 +0900

    feat: 기본페이지 추가

--- mission1-SPA-Router/src/main.tsx
+++ mission1-SPA-Router/src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import App from './App.tsx'
+// import './index.css'
+import Router from './utils/Router';
+import Route from "./utils/Route";
+import Root from "./pages/Root" 
+import About from "./pages/About"; 
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<Router>
+		<Route path="/" component={<Root />} />
+		<Route path="/about" component={<About />} />
+	</Router>
+);
