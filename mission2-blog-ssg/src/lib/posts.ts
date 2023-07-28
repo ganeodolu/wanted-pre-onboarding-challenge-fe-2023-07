cd75c729b9cb492a181f65ab86cbdfc47b067f63
ganeodolu <ganeodolu@gmail.com>
2023-07-28 22:51:02 +0900

    feat: 초기화면 리스트 보여주기
    타입 오류 존재

---
+++ mission2-blog-ssg/src/lib/posts.ts
@@ -0,0 +1,70 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import { remark } from "remark";
+import remarkHtml from "remark-html";
+
+const postsDirectory = path.join(process.cwd(), "_posts");
+
+interface Post {
+  id: string,
+  title: string,
+  date: string
+}
+
+export function getSortedPostsData() : Post[] {
+	const fileNames = fs.readdirSync(postsDirectory);
+	const allPostsData = fileNames.map((fileName) => {
+		const id = fileName.replace(/\.md$/, "");
+
+		const fullPath = path.join(postsDirectory, fileName);
+		const fileContents = fs.readFileSync(fullPath, "utf8");
+		const matterResult = matter(fileContents);
+
+		return {
+			id,
+			...matterResult.data,
+		};
+	});
+	console.log(allPostsData)
+	return allPostsData.sort((a, b) => {
+		return a.date > b.date ? -1 : 1;
+	});
+}
+
+export async function getPostData(id: string) {
+	const fullPath = path.join(postsDirectory, `${id}.md`);
+	const fileContents = fs.readFileSync(fullPath, "utf8");
+
+	// Use gray-matter to parse the post metadata section
+	const matterResult = matter(fileContents);
+
+	// Use remark to convert markdown into HTML string
+	const processedContent = await unified()
+		.use(remarkParse)
+		// .use(remarkHtml)
+		.process(matterResult.content);
+    console.log(processedContent)
+	const contentHtml = processedContent.toString();
+
+	// Combine the data with the id and contentHtml
+	return {
+		id,
+		contentHtml,
+		...matterResult.data,
+	};
+}
+
+export function getAllPostIds() {
+	const fileNames = fs.readdirSync(postsDirectory);
+
+	return fileNames.map((fileName) => {
+		return {
+			params: {
+				id: fileName.replace(/\.md$/, ""),
+			},
+		};
+	});
+}
